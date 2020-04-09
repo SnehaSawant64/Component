f14e6d267f6c7c76bdfeb6e822121696251b6921
Sneha <scsawant1997@gmail.com>
2020-04-10 00:37:57 +0530

    making it responsive

--- components/clock-analog/src/index.js
+++ components/clock-analog/src/index.js
@@ -2,9 +2,9 @@ const clock = document.createElement('template');
 clock.innerHTML = `
 <div class="container">
 	<div class="clock">
-		<img id="hourHand" class="hands" src="https://raw.githubusercontent.com/SnehaSawant64/webcomponents/master/components/clock-analog/images/hour_hand.png" alt="">
-		<img id="minuteHand" class="hands" src="https://raw.githubusercontent.com/SnehaSawant64/webcomponents/master/components/clock-analog/images/minute_hand.png" alt="">
-		<img id="secondHand" class="hands" src="https://raw.githubusercontent.com/SnehaSawant64/webcomponents/master/components/clock-analog/images/second_hand.png" alt="">
+		<img id="hourHand" class="hands" src="https://github.com/XSneha/web-components/blob/master/components/clock-analog/images/hour_hand.png?raw=true" alt="">
+		<img id="minuteHand" class="hands" src="https://raw.githubusercontent.com/XSneha/web-components/master/components/clock-analog/images/minute_hand.png" alt="">
+		<img id="secondHand" class="hands" src="https://raw.githubusercontent.com/XSneha/web-components/master/components/clock-analog/images/second_hand.png" alt="">
 	</div>
 </div>
 <style>
@@ -15,17 +15,18 @@ clock.innerHTML = `
 }
 .clock {
 	position: relative;
-	width: 400px;
-	height: 400px;
-	background: url(https://raw.githubusercontent.com/SnehaSawant64/webcomponents/master/components/clock-analog/images/clock_face.jpg);
+	width: 300px;
+	height: 300px;
+//	background: url(https://raw.githubusercontent.com/XSneha/web-components/master/components/clock-analog/images/clock_face.jpg?raq=true);
+	background : LEMONCHIFFON;
 	border-radius: 50%;
 }
 .clock:after {
 	position: absolute;
 	content: '';
 	background: #000;
-	width: 30px;
-	height: 30px;
+	width: 10px;
+	height: 10px;
 	top: 50%;
 	left: 50%;
 	transform: translateX(-50%) translateY(-50%);
@@ -36,14 +37,13 @@ clock.innerHTML = `
   top: 0;
   left: 0;
 }
-
 @media screen and (min-width: 400px)  and ( max-width: 655px ) {
-	img.hands {// width: 200px;	height: 200px; }
-	img.clock { //width: 200px;	height: 200px; }
+	img.hands { width: 250px;	height: 250px; }
+	.clock { width: 250px;	height: 250px; }
 }
 @media screen and (max-width: 399px ){
-	img.hands { //width: 60px; height:60px }
-	img.clock { //width: 60px; height:60px }
+	img.hands { width: 150px; height:150px }
+	.clock { width: 150px; height:150px }
 }
 </style>
 `;
